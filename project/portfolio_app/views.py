3012b2a12589e90431cfa96b1b0a3704b219952d
gabrielainza <139720159+gabrielainza@users.noreply.github.com>
2023-08-02 17:49:27 +0000

    agregando boton nuevo de casa

--- project/portfolio_app/views.py
+++ project/portfolio_app/views.py
@@ -7,6 +7,10 @@ filename = 'static/classification_svc_sklearn_1_2_2.pickle'
 with open(filename, 'rb') as f:
     loaded_model_classification = pickle.load(f)
 
+filename = 'static/regression_rf_sklearn_1_2_2.pickle'
+with open(filename, 'rb') as f:
+    loaded_model_regression = pickle.load(f)
+
 # Vista Index
 def index(request):
     template = 'portfolio_app/index.html'
@@ -40,5 +44,28 @@ def iris(request):
     template = 'portfolio_app/iris.html'
     context = {'pred_label': pred_label, 
                'pred_image_name': pred_image_name}
+    
+    return render(request, template, context)
+
+
+# Vista Casa
+def casa(request):
+
+    pred_label = None
+
+    if request.method == "POST":
+
+        overall_quality = int(request.POST.get('OverallQual'))
+        above_ground_living_area = int(request.POST.get('GrLivArea'))
+        first_floor_sf = int(request.POST.get('1stFlrSF'))
+        full_bath = int(request.POST.get('FullBath'))
+        year_built = int(request.POST.get('YearBuilt'))
+
+        lista_casa = [overall_quality, above_ground_living_area, first_floor_sf, full_bath, year_built]
+        x_new = np.array([lista_casa])
+        y_pred = float(loaded_model_regression.predict(x_new))   
+
+    template = 'portfolio_app/casa.html'
+    context = {'pred_label': y_pred}
     
     return render(request, template, context)
