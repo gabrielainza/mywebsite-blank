246e1ce74446b1912dad5dff8ff8da514069e285
Gabriel Ainza <139720159+gabrielainza@users.noreply.github.com>
2023-10-09 21:10:36 +0000

    tratando de arreglar bug

--- project/portfolio_app/urls.py
+++ project/portfolio_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('iris/', views.iris, name='iris'),
     path('casa/', views.casa, name='casa'),
+    path('GA_CV.ENG.pdf', views.index, name='cv')
 ]
